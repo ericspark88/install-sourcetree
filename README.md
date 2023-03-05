bb8c8803ed1ebd25ae3c1140782605d637209d6c
Eric Park <ericscottpark@gmail.com>
2023-03-05 12:13:27 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # install-sourcetree
 PowerShell Script to Install SourceTree
+
+# How To Run
+First make sure you can run PowerShell scripts. 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted'
+Then run install_sourcetree.ps1
+
+Defaults to version 3.4.11 but can accept 1 argument with the version wanted.
---
+++ install_sourcetree.ps1
@@ -0,0 +1,8 @@
+#HEY! LISTEN! Do this first, 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted'
+Param([string]$versionToInstall="3.4.11")
+$null = New-Item -ItemType Directory -Force -Path C:\temp
+Write-Host Downloading SourceTree-$versionToInstall... Please Wait! `(Progress bar disabled for speed`)
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-$versionToInstall.exe -OutFile C:\temp\SourceTreeSetup-$versionToInstall.exe -UseBasicParsing
+Start-Process -Wait -FilePath C:\temp\SourceTreeSetup-$versionToInstall.exe -ArgumentList "-s"
+Remove-Item C:\temp\SourceTreeSetup-$versionToInstall.exe
