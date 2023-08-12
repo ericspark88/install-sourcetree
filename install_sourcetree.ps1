da2e52270193cde430fe3350c163148d5e8b2d52
Eric Park <ericscottpark@gmail.com>
2023-08-12 02:37:45 -0400

    Add plink suggestion

--- install_sourcetree.ps1
+++ install_sourcetree.ps1
@@ -6,3 +6,4 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-$versionToInstall.exe -OutFile C:\temp\SourceTreeSetup-$versionToInstall.exe -UseBasicParsing
 Start-Process -Wait -FilePath C:\temp\SourceTreeSetup-$versionToInstall.exe -ArgumentList "-s"
 Remove-Item C:\temp\SourceTreeSetup-$versionToInstall.exe
+echo "After first run of SourceTree run $HOME\AppData\Local\SourceTree\app-3.4.11\tools\putty\plink.exe ericspark88@github.com"
